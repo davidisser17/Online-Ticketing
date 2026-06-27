3b661d95a1acf5f1a826e91ccd798a8fe0e45cdf
davidisser17 <davidisser17@users.noreply.github.com>
2026-06-27 01:08:14 +0000

    deploy: 956640e9c5b19dee78fdec361b5c82afb8fda9e9

---
+++ assets/StatusLabel-OSjrExJD.js
@@ -0,0 +1,1 @@
+import{j as n,F as t,G as a,H as o}from"./index-CWwvapI_.js";function l({status:e,className:S=""}){const s=o.includes(e)?t[e]:a[e];return n.jsx("span",{className:`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${s} ${S}`,"aria-label":`Status: ${e}`,children:e})}export{l as S};
